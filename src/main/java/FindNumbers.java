2b69b0917ea779a4e1960c6c6700d008722b9166
Tsivka Galina <galinatsivka@gmail.come>
2020-07-27 03:34:25 +0300

    dicision

---
+++ src/main/java/FindNumbers.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class FindNumbers {
+
+    /**
+     * <p>Реализуйте метод getAllNumbers(String text) который принимает строку, cодержащую буквы,
+     * целые числа и иные символы.</p>
+     * <p>Требуется: все числа, которые встречаются в строке, поместить в отдельный целочисленный
+     * массив, каждый элемент этого массива умножить на 2. Метод должен возвращать этот массив.</p>
+     * <p>Пример: если дана строка "data 48 call 9 read13 blank0a", то в массиве должны оказаться
+     * числа 92, 18, 26 и 0.</p>
+     */
+    public int[] getAllNumbers(String text) {
+        String temp = text.replaceAll("[^0-9 -]", " ");
+        String[] tempStringArry = temp.split(" ");
+        ArrayList<Integer> rezult = new ArrayList<>();
+        for (int i = 0; i < tempStringArry.length; i++) {
+            if (!tempStringArry[i].isEmpty()) {
+                rezult.add(Integer.parseInt(tempStringArry[i]) * 2);
+            }
+        }
+        int[] dicision = new int[rezult.size()];
+        int j = 0;
+        for (Integer namber : rezult) {
+            dicision[j] = namber;
+            j++;
+        }
+        return dicision;
+    }
+}
